d10259eaea7481bd806afe3f0bbf34f85dfddfa5
TOCOPREGUICA <felipeborgesmelo80@gmail.com>
2025-06-15 04:26:45 -0300

    arrumei os graficos e o km mas o grafico de linha nao ficou bom

--- greenlog-front arrumado/src/app/menu/dashboard/dashboard.component.ts
+++ greenlog-front arrumado/src/app/menu/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { GraphComponent } from "../graph/graph.component";
 import { GraficoLinhaComponent } from "../graficos/grafico-linha/grafico-linha.component";
 import { GraficoPizzaComponent } from "../graficos/grafico-pizza/grafico-pizza.component";
 import { NgxEchartsModule } from 'ngx-echarts';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,9 +12,12 @@ import { NgxEchartsModule } from 'ngx-echarts';
     NgxEchartsModule,
     GraphComponent,
     GraficoLinhaComponent,
-    GraficoPizzaComponent
+    GraficoPizzaComponent,
+    FormsModule
   ],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  anoSelecionado = 2025;
+}
